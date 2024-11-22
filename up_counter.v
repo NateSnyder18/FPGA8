cb429f46b597c20cc1291684d2f3f4d63d5d5ab1
NateSnyder18 <123413999+NateSnyder18@users.noreply.github.com>
2024-11-22 16:47:37 -0500

    Update up_counter.v

--- up_counter.v
+++ up_counter.v
@@ -1,12 +1,19 @@
-module up_counter(input en, input clk, output reg [11:0] count);
+module up_counter(input en, input clk, output [11:0] bin_cnt, output done);
+   reg [11:0] bin;
+   reg rdy;
    
-    initial begin
-        count <= 12'b000000000000;
-    end
+   initial begin
+       bin <= 12'b0;
+   end
    
-    always @ (posedge clk) begin
-        if (en) begin
-            count <= count + 1;
-        end
-    end
+   always@(posedge clk)
+       begin
+           rdy <= 1'b0;
+           if(en)begin
+               bin <= bin + 1;
+               rdy <= 1'b1;
+           end
+       end
+   assign bin_cnt = bin;
+   assign done = rdy;
 endmodule
